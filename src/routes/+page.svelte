e1693f2093fac6ace74bac8188c656b64d6b1b4e
trav164 <travis164@gmail.com>
2023-07-17 20:57:26 +0100

    added some credits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,11 @@
 <script>
 	import Generator from '$lib/components/Generator.svelte';
 	import PasswordDisplay from '$lib/components/PasswordDisplay.svelte';
-	import { password, settings, passwordLength } from '../password';
 </script>
+
+<svelte:head>
+	<title>Password Generator</title>
+</svelte:head>
 
 <main class="flex flex-col gap-6">
 	<h1 class="mx-auto text-2xl text-zinc-600">Password Generator</h1>
@@ -12,4 +15,61 @@
 	<pre class="text-white">{JSON.stringify($settings)}</pre> -->
 	<PasswordDisplay />
 	<Generator />
+
+	<p class="text-zinc-300">
+		Created by <a href="lewistravis.co.uk" class="font-semibold text-white hover:text-emerald-300"
+			>Lewis Travis</a
+		>. Inspired by the challenge on
+		<a
+			class="font-semibold text-white hover:text-emerald-300"
+			href="https://www.frontendmentor.io/challenges/password-generator-app-Mr8CLycqjh"
+			>Frontend Mentor</a
+		>
+	</p>
+
+	<div class=" flex w-full flex-row items-center justify-center gap-6">
+		<a class="transition-all duration-300 hover:-translate-y-1" href="https://github.com/trav164">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="icon icon-tabler icon-tabler-brand-github text-zinc-300 hover:text-emerald-300"
+				width="32"
+				height="32"
+				viewBox="0 0 24 24"
+				stroke-width="2"
+				stroke="currentColor"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path
+					d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5"
+				/>
+			</svg>
+		</a>
+		<a
+			class="transition-all duration-300 hover:-translate-y-1"
+			href="https://www.linkedin.com/in/lewis-travis-43656ab5/"
+		>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="icon icon-tabler icon-tabler-brand-linkedin text-zinc-300 hover:text-emerald-300"
+				width="32"
+				height="32"
+				viewBox="0 0 24 24"
+				stroke-width="2"
+				stroke="currentColor"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z" />
+				<path d="M8 11l0 5" />
+				<path d="M8 8l0 .01" />
+				<path d="M12 16l0 -5" />
+				<path d="M16 16v-3a2 2 0 0 0 -4 0" />
+			</svg>
+		</a>
+	</div>
 </main>
